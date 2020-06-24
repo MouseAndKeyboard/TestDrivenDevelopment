5d9fab52bff9c5b9e68d7229c71ece7528b05134
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-06-25 00:21:28 +0800

    setup+teardown code for the module

--- setup_teardown/test_file.py
+++ setup_teardown/test_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+# Will be called when the module first loads
+def setup_module(module):
+    print("setting up the module")
+
+# Will be called once all the unit tests have been completed.
+def teardown_module(module):
+    print("tearing down the module")
+
 # This will be called before the test
 def setup_function(fn):
     if fn == test1:
