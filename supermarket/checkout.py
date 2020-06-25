bdee8d65cf00149cd2970fc4207dccc250893b89
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-06-25 09:44:58 +0800

    added code to apply discounts

--- supermarket/checkout.py
+++ supermarket/checkout.py
@@ -1,18 +1,42 @@
 #!/usr/bin/env python3
 
 class Checkout:
+
+    class Discount:
+        def __init__(self, nbrItems, price):
+            self.nbrItems = nbrItems
+            self.price = price
+
     def __init__(self):
         self.prices = {}
-        self.total = 0
+        self.discounts = {}
+        self.items = {}
 
     def addItemPrice(self, itemName, price):
         self.prices[itemName] = price
 
     def addItem(self, itemName):
-        self.total += self.prices[itemName]
+        if itemName in self.items:
+            self.items[itemName] += 1
+        else:
+            self.items[itemName] = 1
 
     def calculateTotal(self):
-        return self.total
+        total = 0
+        for item, count in self.items.items():
+            if item in self.discounts:
+                discount = self.discounts[item]
+                if count >= discount.nbrItems:
+                    nbrOfDiscounts = count / discount.nbrItems
+                    total += nbrOfDiscounts * discount.price
+                    remaining = count % discount.nbrItems
+                    total += remaining * self.prices[item]
+                else:
+                    total += self.prices[item] * count
+            else:
+                total += self.prices[item] * count
+        return total
 
     def addDiscount(self, item, nbrItems, price):
-        pass
+        discount = self.Discount(nbrItems, price)
+        self.discounts[item] = discount
