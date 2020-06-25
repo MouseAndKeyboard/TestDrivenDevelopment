b4c4b572d3e5a2c80d8425dd7ec4509dbe6035d3
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-06-25 20:06:07 +0800

    implemented checking if file exists

--- line_reader/LineReader.py
+++ line_reader/LineReader.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+import os
 
 def readFromFile(fileName):
+    if not os.path.exists(fileName):
+        raise Exception("Does not exist")
     f = open(fileName, "r")
     line = f.readline()
     return line
--- line_reader/TestDoubles_Tests.py
+++ line_reader/TestDoubles_Tests.py
@@ -3,11 +3,14 @@ from LineReader import readFromFile
 from unittest.mock import MagicMock
 import pytest
 
+
 def test_returnsCorrectString(monkeypatch):
     mock_file = MagicMock()
     mock_file.readline = MagicMock(return_value="test line")
     mock_open = MagicMock(return_value=mock_file)
     monkeypatch.setattr("builtins.open", mock_open)
+    mock_exists = MagicMock(return_value=True)
+    monkeypatch.setattr("os.path.exists", mock_exists)
     result = readFromFile("file")
     mock_open.assert_called_once_with("file", "r")
     assert result == "test line"
