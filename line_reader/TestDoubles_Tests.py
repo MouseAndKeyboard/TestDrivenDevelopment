65df838e197dcdc43807c40773b2b8d26ec8bb3d
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-06-25 20:01:51 +0800

    test for checking for no file

--- line_reader/TestDoubles_Tests.py
+++ line_reader/TestDoubles_Tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from LineReader import readFromFile
 from unittest.mock import MagicMock
+import pytest
 
 def test_returnsCorrectString(monkeypatch):
     mock_file = MagicMock()
@@ -10,3 +11,13 @@ def test_returnsCorrectString(monkeypatch):
     result = readFromFile("file")
     mock_open.assert_called_once_with("file", "r")
     assert result == "test line"
+
+def test_throwsExceptionWhenNoFile(monkeypatch):
+    mock_file = MagicMock()
+    mock_file.readline = MagicMock(return_value="test line")
+    mock_open = MagicMock(return_value=mock_file)
+    monkeypatch.setattr("builtins.open", mock_open)
+    mock_exists = MagicMock(return_value=False)
+    monkeypatch.setattr("os.path.exists", mock_exists)
+    with pytest.raises(Exception):
+        result = readFromFile("fake file")
