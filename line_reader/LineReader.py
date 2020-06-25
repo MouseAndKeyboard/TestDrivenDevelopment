d30d5a14e91762352e889bbf442ce2095557b313
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-06-25 19:52:20 +0800

    test to see if correct string is loaded

--- line_reader/LineReader.py
+++ line_reader/LineReader.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
-def readFromFile():
+def readFromFile(fileName):
     pass
--- line_reader/TestDoubles_Tests.py
+++ line_reader/TestDoubles_Tests.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 from LineReader import readFromFile
+from unittest.mock import MagicMock
 
 def test_canCallReadFromFile():
     readFromFile("file")
+
+def test_returnsCorrectString(monkeypatch):
+    mock_file = MagicMock()
+    mock_file.readline = MagicMock(return_value="test line")
+    mock_open = MagicMock(return_value=mock_file)
+    monkeypatch.setattr("builtins.open", mock_open)
+    result = readFromFile("file")
+    mock_open.assert_called_once_with("file", "r")
+    assert result == "test line"
