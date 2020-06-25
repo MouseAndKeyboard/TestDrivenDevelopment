e8d162c42162ff0b16f93c80c0fc5d244427ff1f
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-06-25 08:54:10 +0800

    additemprice test

--- supermarket/TestCheckout.py
+++ supermarket/TestCheckout.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+from checkout import Checkout
 
 def test_CanInstantiateCheckout():
     co = Checkout()
+
+def test_canAddItemPrice():
+    co = Checkout()
+    co.addItemPrice("a", 1)
