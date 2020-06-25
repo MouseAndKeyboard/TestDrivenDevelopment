cd6926f5335889aedaa202180d32d998a6f234e7
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-06-25 19:47:21 +0800

    implemented readfromfile

--- line_reader/LineReader.py
+++ line_reader/LineReader.py
@@ -1,2 +1,4 @@
 #!/usr/bin/env python3
 
+def readFromFile():
+    pass
--- line_reader/TestDoubles_Tests.py
+++ line_reader/TestDoubles_Tests.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from LineReader import readFromFile
 
 def test_canCallReadFromFile():
     readFromFile()
