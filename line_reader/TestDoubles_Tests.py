5ddcfe859f73df6207c7dfb04af87c7fc53342f5
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-06-25 20:09:01 +0800

    refactored test code

--- line_reader/TestDoubles_Tests.py
+++ line_reader/TestDoubles_Tests.py
@@ -3,23 +3,22 @@ from LineReader import readFromFile
 from unittest.mock import MagicMock
 import pytest
 
+@pytest.fixture
+def mock_open(monkeypatch):
+   mock_file = MagicMock()
+   mock_file.readline = MagicMock(return_value="test line")
+   mock_open = MagicMock(return_value=mock_file)
+   monkeypatch.setattr("builtins.open", mock_open)
+   return mock_open
 
-def test_returnsCorrectString(monkeypatch):
-    mock_file = MagicMock()
-    mock_file.readline = MagicMock(return_value="test line")
-    mock_open = MagicMock(return_value=mock_file)
-    monkeypatch.setattr("builtins.open", mock_open)
+def test_returnsCorrectString(mock_open, monkeypatch):
     mock_exists = MagicMock(return_value=True)
     monkeypatch.setattr("os.path.exists", mock_exists)
     result = readFromFile("file")
     mock_open.assert_called_once_with("file", "r")
     assert result == "test line"
 
-def test_throwsExceptionWhenNoFile(monkeypatch):
-    mock_file = MagicMock()
-    mock_file.readline = MagicMock(return_value="test line")
-    mock_open = MagicMock(return_value=mock_file)
-    monkeypatch.setattr("builtins.open", mock_open)
+def test_throwsExceptionWhenNoFile(mock_open, monkeypatch):
     mock_exists = MagicMock(return_value=False)
     monkeypatch.setattr("os.path.exists", mock_exists)
     with pytest.raises(Exception):
