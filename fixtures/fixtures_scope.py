c56fb65606e490cee4dc74eaeb5562db6ec19dfb
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-06-25 08:38:18 +0800

    fixed scopes

--- fixtures/fixtures_scope.py
+++ fixtures/fixtures_scope.py
@@ -6,14 +6,15 @@ def setupSession():
     print('setting up session')
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="module", autouse=True)
 def setupModule():
     print('setting up the module')
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def setupFunction():
     print('setting up the function')
+
 
 def test1():
     print('running first test')
